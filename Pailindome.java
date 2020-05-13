d0104cb01f34cda78d648b73af006411429ed17b
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-05-13 22:08:00 +0700

    Add files via upload

---
+++ Pailindome.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.Stack;
+
+public class Pailindome {
+        public static void main(String[] args) {
+            Stack<StringBuffer> stack = new Stack<>();
+            String palindrome = "able was I ere I saw elba";
+            StringBuffer stringBuffer = new StringBuffer();
+            for (int i = palindrome.length()-1; i>=0; i--){
+                stringBuffer=stringBuffer.append(palindrome.charAt(i));
+            }
+            stack.push(stringBuffer);
+            StringBuffer str = stack.pop();
+
+            if (palindrome.equalsIgnoreCase(String.valueOf(str))){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
